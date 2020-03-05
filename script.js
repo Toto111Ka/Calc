b324658210045ed7a794050d2c228358c40e1909
Toto111Ka <54114406+Toto111Ka@users.noreply.github.com>
2020-03-06 02:23:27 +0300

    Fix

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ let inputUsd = document.getElementById('usd'),
 inputRub.addEventListener('input', () => {
     let request = new XMLHttpRequest();
 
-    request.open('GET', 'current.json');
+    request.open("GET", "current.json");
     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
     
     request.addEventListener('readystatechange', function(){
         if (request.readyState === 4 && request.status == 200) {
